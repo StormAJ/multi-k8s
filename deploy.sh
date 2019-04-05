eac72a08a4330fcda9f0f196f778693664e84571
Storm <heike.storm@web.de>
2019-04-05 18:08:16 +0200

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t arwedstorm/multi-client:latest -t arwedstorm/multi-client:$SHAW -f ./client/Dockerfile ./client
+docker build -t arwedstorm/multi-server:latest -t arwedstorm/multi-server:$SHAW -f ./server/Dockerfile ./server
+docker build -t arwedstorm/multi-worker:latest -t arwedstorm/multi-worker:$SHAW -f ./worker/Dockerfile ./worker
+
+docker push arwedstorm/multi-client:latest
+docker push arwedstorm/multi-server:latest
+docker push arwedstorm/multi-worker:latest
+
+docker push arwedstorm/multi-client:$SHA
+docker push arwedstorm/multi-server:$SHA
+docker push arwedstorm/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=arwedstorm/multi-server:$SHA
+kubectl set image deployments/client-deployment client=arwedstorm/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=arwedstorm/multi-worker:$SHA
